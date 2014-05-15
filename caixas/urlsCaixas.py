b46091c321ac3a4f0e45d076da9bf9aab03d9f62
Thiago22 <thiago5046@hotmail.com>
2014-05-15 20:44:31 -0300

    Todo Projeto

---
+++ caixas/urlsCaixas.py
@@ -0,0 +1,15 @@
+""" 
+
+https://www.facebook.com/groups/pythonmania/
+"""
+
+from django.conf.urls import patterns, include, url
+# Interligando Url's
+urlpatterns = patterns('caixas.views',
+    url(r'^adicionar/$', 'caixaAdicionar'),
+    url(r'^editar/(?P<pk>\d+)/$', 'caixaEditar'),
+    url(r'^salvar/$', 'caixaSalvar'),
+    url(r'^pesquisar/$', 'caixaPesquisar'),
+    url(r'^excluir/(?P<pk>\d+)/$', 'caixaExcluir'),
+    url(r'^$', 'caixaListar'),
+)
